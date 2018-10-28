316586da8d17db021028c25000f593335d0064b3
opeendoing <dyninger@163.com>
2018-10-28 21:59:35 +0800

    编辑家庭成员页面

---
+++ src/pages/patient/EditPatient.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+      <Header :tittle="tittle"></Header>
+    </div>
+</template>
+
+<script>
+import Header from './components/Header'
+export default {
+  name: 'EditPatient',
+  components: {
+    Header
+  },
+  data () {
+    return {
+      tittle: '编辑家庭成员'
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
