9687a1e41cc3a917949986f818ec8e373ac63b76
opeendoing <dyninger@163.com>
2018-10-28 21:59:21 +0800

    添加家庭成员页面

---
+++ src/pages/patient/AddPatient.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+      <Header :tittle="tittle"></Header>
+    </div>
+</template>
+
+<script>
+import Header from './components/Header'
+export default {
+  name: 'AddPatient',
+  components: {
+    Header
+  },
+  data () {
+    return {
+      tittle: '添加家庭成员'
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
