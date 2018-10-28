f759b3932caebee68a06f327ba2018baf2159a7e
opeendoing <dyninger@163.com>
2018-10-28 21:59:50 +0800

    成员列表页面

---
+++ src/pages/patient/PatientsList.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <Header :tittle="tittle"></Header>
+    <ListBody></ListBody>
+  </div>
+</template>
+
+<script>
+import Header from './components/Header'
+import ListBody from './components/ListBody'
+export default {
+  name: 'PatientsList',
+  components: {
+    Header,
+    ListBody
+  },
+  data () {
+    return {
+      tittle: '家庭成员管理'
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
